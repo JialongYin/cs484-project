87c4cec4dbdd9d9aac07eb6d484ac22b6c5c984e
JialongYin <yinjlusa@gmail.com>
2022-05-04 07:18:57 +0800

    first commit

--- src/solution.cpp
+++ src/solution.cpp
@@ -21,9 +21,10 @@ void rebalance(const dist_sort_t *data, const dist_sort_size_t myDataCount, dist
 		int rank, nprocs;
     MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+		std::cout << "nprocs:" << nProcs << ";rank:" << rank << std::endl << std::flush;
 		dist_sort_size_t global_N;
 		MPI_Allreduce(&myDataCount, &global_N, 1, MPI_TYPE_DIST_SORT_SIZE_T, MPI_SUM, MPI_COMM_WORLD);
+		std::cout << "global_N:" << global_N << ";myDataCount:" << myDataCount << ";rank:" << rank << std::endl << std::flush;
 		if (rank < nprocs-1) {
 				*rCount = ceil(global_N/nprocs);
 		} else {
@@ -33,7 +34,7 @@ void rebalance(const dist_sort_t *data, const dist_sort_size_t myDataCount, dist
 
 		dist_sort_size_t global_count = 0;
     MPI_Exscan(&myDataCount, &global_count, 1, MPI_TYPE_DIST_SORT_SIZE_T, MPI_SUM, MPI_COMM_WORLD);
-
+		std::cout << "global_count:" << global_count << ";rank:" << rank << std::endl << std:flush;
     MPI_Win win;
     MPI_Win_create(*rebalancedData, (*rCount) * sizeof(dist_sort_t), sizeof(dist_sort_t), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
     MPI_Win_fence(MPI_MODE_NOPRECEDE, win); //fence - there are no epochs before this
@@ -43,7 +44,7 @@ void rebalance(const dist_sort_t *data, const dist_sort_size_t myDataCount, dist
     {
         int target_rank = int((global_count+i) / max_size);
 				if (target_rank == 4) {
-						std::cout << global_count+i << "," << max_size <<std::endl << std::flush;
+						std::cout << global_count+i << "," << max_size << std::endl << std::flush;
 				}
 				// std::cout << target_rank << "," << global_count << "," << i << "," << max_size <<std::endl << std::flush;
         int displacement = (global_count+i) % max_size;
