bc2b6f046508f14fc062bc18e2b3fc8b73eae63f
JialongYin <yinjlusa@gmail.com>
2022-05-15 11:34:26 +0800

    first commit

--- src/solution.cpp
+++ src/solution.cpp
@@ -165,10 +165,10 @@ void findSplitters(const dist_sort_t *data, const dist_sort_size_t data_size, di
 										lowerBound[i] = splitters[i];
 										upperBound[i] = splitters[i];
 								}
-								new_splitters[i] = (lowerBound[i] + upperBound[i]) / 2.0;
+								new_splitters[i] = lowerBound[i] + ((upperBound[i] - lowerBound[i]) / 2);
 								std::cerr << "lowerBound" << i << ":" << lowerBound[i]/DEBUG << ":rank:" << rank << std::endl;
 								std::cerr << "upperBound" << i << ":" << upperBound[i]/DEBUG << ":rank:" << rank << std::endl;
-								std::cerr << "new_splitters" << i << ":" << new_splitters[i]/DEBUG << ":rank:" << rank << std::endl;
+								// std::cerr << "new_splitters" << i << ":" << new_splitters[i]/DEBUG << ":rank:" << rank << std::endl;
 						}
 						for (dist_sort_size_t i = 0; i < numSplitters-1; ++i) {
 									splitters[i] = new_splitters[i];
